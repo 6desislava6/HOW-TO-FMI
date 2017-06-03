899ae2baf0a218592f7e35642ef2f3183f09398d
6desislava6 <desislavatsvetkova@mail.bg>
2017-06-03 21:15:47 +0300

    Dropdown added

--- How-to-FMI/app/js/controllers/how-to-fmi-controller.js
+++ How-to-FMI/app/js/controllers/how-to-fmi-controller.js
@@ -1,6 +1,19 @@
 'use strict';
 angular.module('htfmi')
     .controller('HowToFMIController', ['$scope', function ($scope) {
-    $scope.team = ['Kremena', 'Tonka'];
-  	
+
+	// Close the dropdown menu if the user clicks outside of it
+	window.onclick = function(event) {
+	  if (!event.target.matches('.dropbtn')) {
+
+	    var dropdowns = document.getElementsByClassName("dropdown-content");
+	    var i;
+	    for (i = 0; i < dropdowns.length; i++) {
+	      var openDropdown = dropdowns[i];
+	      if (openDropdown.classList.contains('show')) {
+	        openDropdown.classList.remove('show');
+	      }
+	    }
+	  }
+	}
 }]);	
