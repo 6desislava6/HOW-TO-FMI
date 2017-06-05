46cd63f942efef28591444acde6ce1483832848b
6desislava6 <desislavatsvetkova@mail.bg>
2017-06-05 08:16:24 +0300

    Course discussion

--- How-to-FMI/app/js/controllers/course-controller.js
+++ How-to-FMI/app/js/controllers/course-controller.js
@@ -17,10 +17,17 @@ angular.module('htfmi')
 				image: 'elixir.png'
 			}
 		}
-
+		$scope.showContent = {};
+		$scope.showContent['annotation'] = true;
 		$scope.course = courses[id];
 		$scope.changeCourseContent = function (contentName) {
 			$scope.showContent = {};
 			$scope.showContent[contentName] = true;
 		};
+
+		$scope.discussion = [
+			{rating: 1, content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil"},
+			{rating: 10, content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil"},
+			{rating: 6, content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil"}
+		]
 }]);
