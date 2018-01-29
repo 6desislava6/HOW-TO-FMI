0e4284a60fa26772cab7d9974303c217c13c91f8
6desislava6 <desislavatsvetkova@mail.bg>
2018-01-29 21:26:25 +0200

    Susi resource.

--- How-to-FMI/app/js/services/resources.js
+++ How-to-FMI/app/js/services/resources.js
@@ -10,4 +10,10 @@ angular.module('htfmi')
 .factory('Courses', ['$resource', function ($resource) {
 	// using rest API here.
     return null;
+}])
+.factory('SusiResource', ['$resource', function ($resource) {
+	var resourceLink = 'http://0.0.0.0:5000/susi/:susiUser/:susiPassword';
+    return $resource(resourceLink, {susiUser:'@susiUser', susiPassword: '@susiPassword'}, {
+    	get: {method: 'GET'},
+    });
 }]);
